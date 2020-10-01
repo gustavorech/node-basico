4659fb71641294b663ddc7ec705db6388635dc98
Gustavo Rech <rech.gustvo@hotmail.com>
2020-10-01 17:56:45 -0300

    Passo 4 body parser e cors

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (request.method === 'OPTIONS') {
+        response.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return response.status(200).json({});
+    }
+
+    next();
+});
 
 const productRouts = require('./api/routes/products');
 const orderRouts = require('./api/routes/orders');
@@ -23,6 +38,6 @@ app.use((error, request, response, next) => {
             message: error.message
         }
     })
-})
+});
 
 module.exports = app;
